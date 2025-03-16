b431952b6f3c103b40a60d4e5c354f0609b932f2
JesusECB <jecb.st@gmail.com>
2025-03-16 15:58:34 +0100

    Actualización: ajuste de estructura y configuración del ejercicio

--- workspace/littlelemon/restaurant/models.py
+++ workspace/littlelemon/restaurant/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+# models.py
 # Create your models here.
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
