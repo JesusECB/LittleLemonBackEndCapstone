64987986d0d9129db1c3b681f03f5c7efd1222f7
JesusECB <jecb.st@gmail.com>
2025-03-18 04:54:18 +0100

    Implementa modelos Menu y Booking, registra en admin y crea superusuario

--- workspace/littlelemon/restaurant/models.py
+++ workspace/littlelemon/restaurant/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 # models.py
 # Create your models here.
-class MenuItem(models.Model):
-    name = models.CharField(max_length=100)
+class Menu(models.Model):
+    title = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    description = models.TextField(blank=True, null=True)
+    inventory = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+class Booking(models.Model):
+    name = models.CharField(max_length=100)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField()
 
     def __str__(self):
         return self.name
