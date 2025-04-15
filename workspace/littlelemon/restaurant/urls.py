dbac44c36cbf537ab6a13386c8a5e3f41576e1d8
JesusECB <jecb.st@gmail.com>
2025-04-15 04:05:46 +0200

    Implementa Menu and Booking APIs with DRF and configures router

--- workspace/littlelemon/restaurant/urls.py
+++ workspace/littlelemon/restaurant/urls.py
@@ -1,7 +1,16 @@
-#define URL route for index() view
-from django.urls import path
+# restaurant/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from restaurant import views as restaurant_views
 from . import views
 
+router = DefaultRouter()
+router.register(r'tables', restaurant_views.BookingViewSet)
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('booking/', include(router.urls)),
+    path('', restaurant_views.index, name='index'),
+    path('api/menu/', restaurant_views.MenuItemView.as_view(), name='menu-list'),
+    path('api/menu/<int:pk>/', restaurant_views.SingleMenuItemView.as_view(), name='menu-detail'),
 ]
--- workspace/littlelemon/restaurant/views.py
+++ workspace/littlelemon/restaurant/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
-# Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class BookingViewSet(viewsets.ModelViewSet):
+
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class MenuItemView(generics.ListCreateAPIView):
+    """
+    Esta vista maneja los métodos GET y POST para el modelo Menu:
+    - GET: Listar todos los elementos del menú.
+    - POST: Crear un nuevo elemento del menú.
+    """
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Esta vista maneja los métodos GET, PUT y DELETE para un solo elemento del menú:
+    - GET: Recuperar los detalles de un elemento.
+    - PUT: Actualizar un elemento.
+    - DELETE: Eliminar un elemento.
+    """
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
